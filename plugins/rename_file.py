e76e2475b96c786a06de71b6de5783d528571d2c
GarouTheDevil <106361150+GarouTheDevil@users.noreply.github.com>
2022-07-23 10:42:48 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -40,12 +40,12 @@ from database.db import *
 async def set_caption(bot, update):
     if len(update.command) == 1:
         await update.reply_text(
-            "Custom Caption \n\n you can use this command to set your own caption  \n\n Usage : /scaption Your caption text \n\n note : For current file name use : <code>{filename}</code>", 
+            "Custom Caption : Use This Cammand To Set Custom Caption  \nUsage : /scaption Text \nNote : For Current File Name Use : <code>{filename}</code>", 
             quote = True, 
             reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('Show Current Caption', callback_data = "shw_caption")      
+                    InlineKeyboardButton('Current Caption', callback_data = "shw_caption")      
                 ],
                 [
                     InlineKeyboardButton('Delete Caption', callback_data = "d_caption")
@@ -56,7 +56,7 @@ async def set_caption(bot, update):
     else:
         command, CSTM_FIL_CPTN = update.text.split(' ', 1)
         await update_cap(update.from_user.id, CSTM_FIL_CPTN)
-        await update.reply_text(f"**--Your Caption--:**\n\n{CSTM_FIL_CPTN}", quote=True)
+        await update.reply_text(f"**--Your Caption--:** {CSTM_FIL_CPTN}", quote=True)
 
 
 @Mai_bOTs.on_message(pyrogram.filters.command(["rename"]))
@@ -66,13 +66,13 @@ async def rename_doc(bot, update):
         try:
             user = await bot.get_chat_member(update_channel, update.chat.id)
             if user.status == "kicked":
-               await update.reply_text(" Sorry,You've Been Banned From Using Meh!")
+               await update.reply_text("You Are Banned")
                return
         except UserNotParticipant:
             await update.reply_text(
-                text="**Due To The Huge Traffic Only Channel Members Can Use This Bot Means You Need To Join The Below Mentioned Channel Before Using Me! **",
+                text="**Join Updates Channel**",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+                    [ InlineKeyboardButton(text="Updates Channel", url=f"https://t.me/{update_channel}")]
               ])
             )
             return
@@ -163,7 +163,7 @@ async def rename_doc(bot, update):
                 caption=f"{caption_text2}",
                 parse_mode = "html",
                 reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="𝚂ᴜᴘᴘᴏʀᴛ 𝙲ʜᴀɴɴᴇʟ", url=f"https://t.me/Mai_bOTs")]
+                    [ InlineKeyboardButton(text="Channel", url=f"https://t.me/DevilBotz")]
               ]), 
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
